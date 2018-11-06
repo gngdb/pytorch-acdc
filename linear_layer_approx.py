fc210d07e224ced5e749abaca331d48d16ffeaaa
gngdb <gavingray1729@gmail.com>
2018-11-06 17:17:57 +0000

    add shufflenet block

--- linear_layer_approx.py
+++ linear_layer_approx.py
@@ -7,11 +7,13 @@ import torch.nn.functional as F
 
 from collections import OrderedDict
 from functools import reduce
+from itertools import product
 
 import numpy as np
 from scipy import optimize
 
 from pytorch_acdc.layers import StackedLinearACDC
+from layers import ShuffleNetLinear
 
 from obj import PyTorchObjective
 
@@ -69,7 +71,8 @@ if __name__ == '__main__':
 
     with tqdm(total=6*n_replicates) as pbar:
         # ACDC experiments
-        for i in range(6):
+        #for i in range(6):
+        for i in []:
             n_layers = 2**i
             exp_str = 'ACDC_%i'%n_layers
             pbar.set_description(exp_str)
@@ -86,6 +89,36 @@ if __name__ == '__main__':
                     self.X, self.Y = X, Y
                 def forward(self):
                     output = self.acdc(self.X)
+                    return F.mse_loss(output, self.Y).mean()
+
+            # run experiment n_replicates times
+            for n in range(n_replicates):
+                X,Y = sample_experiment(M, N)
+                objective = Objective(X, Y)
+                mse = run_experiment(X,Y,objective) 
+                results[exp_str] += [(M, N, count_params(objective), mse)]
+                pbar.update(1)
+
+    plan = list(product([int(2**i) for i in range(2)],
+                        [int(2**i) for i in range(1,5)]))
+    with tqdm(total=len(plan)*n_replicates) as pbar:
+        # ShuffleNet experiments
+        for n_layers, n_groups in plan:
+        #for i in []:
+            exp_str = 'ShuffleNet_%i_%i'%(n_layers, n_groups)
+            pbar.set_description(exp_str)
+            if exp_str not in results.keys():
+                results[exp_str] = []
+            # make module executing the experiment
+            class Objective(nn.Module):
+                def __init__(self, X, Y):
+                    super(Objective, self).__init__()
+                    # M layers
+                    self.linapprox = ShuffleNetLinear(M,M,n_layers,n_groups)
+                    self.linapprox = self.linapprox.train()
+                    self.X, self.Y = X, Y
+                def forward(self):
+                    output = self.linapprox(self.X)
                     return F.mse_loss(output, self.Y).mean()
 
             # run experiment n_replicates times
